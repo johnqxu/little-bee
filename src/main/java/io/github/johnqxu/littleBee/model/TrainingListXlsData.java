34985bf58f707aeaf8acd6a9adbfed88f4a87c93
Xu Qing <xuqing827@icloud.com>
2022-06-19 09:33:54 +0800

    增加导出数据功能

--- src/main/java/io/github/johnqxu/littleBee/model/TrainingListXlsData.java
+++ src/main/java/io/github/johnqxu/littleBee/model/TrainingListXlsData.java
@@ -2,6 +2,8 @@ package io.github.johnqxu.littleBee.model;
 
 import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.annotation.format.DateTimeFormat;
+import com.alibaba.excel.annotation.format.NumberFormat;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import com.alibaba.excel.annotation.write.style.HeadFontStyle;
 import lombok.Data;
 
@@ -11,6 +13,7 @@ import java.util.Date;
 @HeadFontStyle(fontHeightInPoints = 10,fontName = "Arial")
 public class TrainingListXlsData {
 
+    @ColumnWidth(3)
     @ExcelProperty("序号")
     private int seq;
 
@@ -18,14 +21,18 @@ public class TrainingListXlsData {
     private String name;
 
     @ExcelProperty("证件类型")
+    @NumberFormat
     private String idType;
 
+    @ColumnWidth(20)
     @ExcelProperty("证件号码")
     private String idNo;
 
+    @ColumnWidth(13)
     @ExcelProperty("手机")
     private String mobile;
 
+    @ColumnWidth(10)
     @ExcelProperty("公司名称")
     private String companyName;
 
@@ -35,24 +42,29 @@ public class TrainingListXlsData {
     @ExcelProperty("是否本市户籍")
     private String isLocal;
 
+    @ColumnWidth(20)
     @ExcelProperty("课程名称")
     private String projectName;
 
     @ExcelProperty("课时")
     private int schoolHours;
 
+    @ColumnWidth(12)
     @DateTimeFormat("yyyy-MM-dd")
     @ExcelProperty("课程开始日期")
     private Date projectStartDate;
 
+    @ColumnWidth(12)
     @DateTimeFormat("yyyy-MM-dd")
     @ExcelProperty("课程结束日期")
     private Date projectEndDate;
 
+    @ColumnWidth(12)
     @DateTimeFormat("yyyy-MM-dd")
     @ExcelProperty("社保开始缴纳日期")
     private Date trainerStartDate;
 
+    @ColumnWidth(12)
     @DateTimeFormat("yyyy-MM-dd")
     @ExcelProperty("社保结束缴纳日期")
     private Date trainerEndDate;
