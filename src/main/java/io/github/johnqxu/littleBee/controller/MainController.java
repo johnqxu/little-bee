13449d176471ed0527582d0e8de35916c2ad7ed9
Xu Qing <xuqing827@icloud.com>
2022-07-28 21:28:24 +0800

    优化数据处理

--- src/main/java/io/github/johnqxu/littleBee/controller/MainController.java
+++ src/main/java/io/github/johnqxu/littleBee/controller/MainController.java
@@ -1,12 +1,13 @@
 package io.github.johnqxu.littleBee.controller;
 
-import io.github.johnqxu.littleBee.event.MessageEvent;
-import io.github.johnqxu.littleBee.event.ProgressChangeEvent;
 import io.github.johnqxu.littleBee.event.StartProcessEvent;
 import io.github.johnqxu.littleBee.service.PerformService;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -21,7 +22,6 @@ import javax.annotation.Resource;
 import java.io.File;
 import java.net.URL;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -116,34 +116,34 @@ public class MainController implements Initializable, ApplicationListener<Applic
         exportBtn.setDisable(false);
     }
 
-    private void show(Alert.AlertType alertType, String message, ButtonType... buttons) {
-        if (alert == null) {
-            alert = new Alert(Alert.AlertType.INFORMATION);
-        }
-        alert.setAlertType(alertType);
-        alert.setContentText(message);
-        alert.getButtonTypes().removeAll(new ArrayList<>(alert.getButtonTypes()));
-        alert.getButtonTypes().addAll(buttons);
-        alert.showAndWait();
-    }
+//    private void show(Alert.AlertType alertType, String message, ButtonType... buttons) {
+//        if (alert == null) {
+//            alert = new Alert(Alert.AlertType.INFORMATION);
+//        }
+//        alert.setAlertType(alertType);
+//        alert.setContentText(message);
+//        alert.getButtonTypes().removeAll(new ArrayList<>(alert.getButtonTypes()));
+//        alert.getButtonTypes().addAll(buttons);
+//        alert.showAndWait();
+//    }
 
     @Override
     public void onApplicationEvent(ApplicationEvent applicationEvent) {
-        if (applicationEvent instanceof ProgressChangeEvent progressChangeEvent) {
-            if (progressChangeEvent.getProgress() > 0) {
-                progressBar.setProgress(progressChangeEvent.getProgress());
-            }
-            String log = progressLog.getText();
-            log = String.join("\n", progressChangeEvent.getProgressText(), log);
-            progressLog.setText(log);
-            if (progressChangeEvent.getProgress() == 1) {
-                startBtn.setDisable(false);
-            }
-        } else if (applicationEvent instanceof MessageEvent validationEvent) {
-            String log = progressLog.getText();
-            log = String.join("\n", validationEvent.getValidateMessage(), log);
-            progressLog.setText(log);
-        }
+//        if (applicationEvent instanceof ProgressChangeEvent progressChangeEvent) {
+//            if (progressChangeEvent.getProgress() > 0) {
+//                progressBar.setProgress(progressChangeEvent.getProgress());
+//            }
+//            String log = progressLog.getText();
+//            log = String.join("\n", progressChangeEvent.getProgressText(), log);
+//            progressLog.setText(log);
+//            if (progressChangeEvent.getProgress() == 1) {
+//                startBtn.setDisable(false);
+//            }
+//        } else if (applicationEvent instanceof MessageEvent validationEvent) {
+//            String log = progressLog.getText();
+//            log = String.join("\n", validationEvent.getValidateMessage(), log);
+//            progressLog.setText(log);
+//        }
     }
 
 
--- src/main/java/io/github/johnqxu/littleBee/service/PerformService.java
+++ src/main/java/io/github/johnqxu/littleBee/service/PerformService.java
@@ -2,28 +2,19 @@ package io.github.johnqxu.littleBee.service;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
-import com.alibaba.excel.read.listener.PageReadListener;
 import com.alibaba.excel.util.ListUtils;
 import com.alibaba.excel.write.metadata.WriteSheet;
-import io.github.johnqxu.littleBee.model.dto.EmployDto;
-import io.github.johnqxu.littleBee.model.dto.ProjectDto;
-import io.github.johnqxu.littleBee.model.dto.SigninDto;
+import io.github.johnqxu.littleBee.model.SupplementaryXlsData;
+import io.github.johnqxu.littleBee.model.TrainingListXlsData;
 import io.github.johnqxu.littleBee.model.entity.EmployEntity;
 import io.github.johnqxu.littleBee.model.entity.ProjectEntity;
-import io.github.johnqxu.littleBee.event.MessageEvent;
-import io.github.johnqxu.littleBee.event.ProgressChangeEvent;
-import io.github.johnqxu.littleBee.exception.IllegalDataException;
-import io.github.johnqxu.littleBee.model.*;
 import io.github.johnqxu.littleBee.repository.EmployRepository;
 import io.github.johnqxu.littleBee.repository.ProjectRepository;
-import javafx.scene.control.Alert;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -48,105 +39,6 @@ public class PerformService {
         this.signinService = signinService;
         this.employRepository = employRepository;
         this.projectRepository = projectRepository;
-    }
-
-    private void setProgress(String progressLog, double progress) {
-        applicationContext.publishEvent(new ProgressChangeEvent(this, progressLog, progress));
-    }
-
-
-    public void initDatabase() {
-        employService.deleteAll();
-        projectService.deleteAll();
-        signinService.deleteAll();
-    }
-
-    private void validate() {
-        try {
-            //校验员工信息
-            employService.validate();
-            //校验项目信息
-            projectService.validate();
-            //校验签到信息
-            signinService.validate();
-
-        } catch (IllegalDataException e) {
-            applicationContext.publishEvent(new MessageEvent(this, e.getMessage(), Alert.AlertType.ERROR));
-        }
-    }
-
-    void assign() {
-        signinService.assign();
-    }
-
-    @Async
-    public void perform(File projectExcel, File employExcel, File projectEmployExcel) {
-        setProgress("开始初始化数据库", 0);
-        initDatabase();
-        setProgress("完成数据库初始化", 0.1);
-
-        //导入项目信息
-        setProgress("开始导入项目数据", 0.1);
-        if (projectExcel != null) {
-            EasyExcel.read(projectExcel, ProjectXlsData.class, new PageReadListener<ProjectXlsData>(dataList -> {
-                for (ProjectXlsData projectData : dataList) {
-                    ProjectDto projectDto = ProjectDto.builder()
-                            .projectName(projectData.getProjectName().trim())
-                            .startDate(projectData.getStartDate())
-                            .endDate(projectData.getEndDate())
-                            .schoolHour(projectData.getSchoolHour())
-                            .priority(projectData.getPriority())
-                            .build();
-                    projectService.create(projectDto);
-                }
-            })).sheet().doRead();
-        }
-        setProgress("项目数据导入完成", 0.2);
-
-        //导入员工花名册
-        setProgress("开始导入员工数据", 0.2);
-        if (employExcel != null) {
-            EasyExcel.read(employExcel, EmployXlsData.class, new PageReadListener<EmployXlsData>(dataList -> {
-                for (EmployXlsData employData : dataList) {
-                    log.info("处理:{}", employData.getEmployName());
-                    EmployDto employDto = EmployDto.builder()
-                            .employName(employData.getEmployName())
-                            .companyName(employData.getCompanyName())
-                            .startDate(employData.getStartDate())
-                            .endDate(employData.getEndDate())
-                            .idNo(employData.getIdNo())
-                            .mobile(employData.getMobile())
-                            .status("normal")
-                            .build();
-                    employService.create(employDto);
-                }
-            })).sheet().doRead();
-        }
-        setProgress("完成员工数据导入", 0.3);
-        setProgress("准备导入签到数据", 0.3);
-        if (projectEmployExcel != null) {
-            EasyExcel.read(projectEmployExcel, SigninXlsData.class, new PageReadListener<SigninXlsData>(dataList -> {
-                for (SigninXlsData signinXlsData : dataList) {
-                    SigninDto signinDto = SigninDto.builder()
-                            .projectName(signinXlsData.getProjectName().trim())
-                            .employName(signinXlsData.getEmployName().trim())
-                            .build();
-                    signinService.create(signinDto);
-                }
-            })).sheet().doRead();
-        }
-        setProgress("完成签到数据导入", 0.4);
-
-        //校验数据
-        setProgress("准备数据校验", 0.4);
-        validate();
-        setProgress("完成数据校验", 0.5);
-
-        //处理数据
-        setProgress("准备数据处理", 0.5);
-        assign();
-        setProgress("完成数据处理", 1.0);
-
     }
 
     public void export() throws ParseException {
